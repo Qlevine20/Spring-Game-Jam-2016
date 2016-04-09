055e3adf52bdcbc72c6e8c6b1d81135816bf3153
Quinn Levine <Qlevine@uci.edu>
2016-04-09 09:10:10 -0700

    Put in a placeholder sprite for the animal and got rid of the Debug.Logs.

---
+++ Assets/Scripts/Animal_DestroybyBoundary.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animal_DestroybyBoundary : MonoBehaviour {
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+
+        Destroy(other.gameObject);
+    }
+}
