4dcaaca407405b075ed2dc926964b6e3d0eb134b
KTZimmerle <kzimmerl@uci.edu>
2016-04-09 13:06:32 -0700

    3 new scripts UNTESTED
    GameManager, UI_Lives, and UI_Score. Will test after lunch.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class GameManager : MonoBehaviour {
+    
+    public static int lives = 3;
+    public static int score = 0;
+    public static float timer = 0.0f;
+    UI_Score UIscore;
+    UI_Lives UIlives;
+
+    void Start()
+    {
+        UIscore = GetComponent<UI_Score>();
+        UIlives = GetComponent<UI_Lives>();
+    }
+
+    //Positive numbers to decrease, negative to increase
+    public void ModifyLives(int value = 1)
+    {
+        lives -= value;
+        UIlives.UpdateLives(value);
+    }
+    
+    //Positive numbers to increase, negative to decrease
+    public void ModifyScore(int points)
+    {
+        score += points;
+        UIscore.UpdateScore(score);
+    }
+}
---
+++ Assets/Scripts/UI_Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Score : MonoBehaviour {
+
+    //int score;
+    Text ScoreText;
+
+    public void UpdateScore(int score)
+    {
+        ScoreText = GetComponent<Text>();
+        ScoreText.text = score.ToString();
+    }
+}
