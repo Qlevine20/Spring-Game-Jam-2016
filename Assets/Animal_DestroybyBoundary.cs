3ec4707064c3aa39c589a74ba0d91e8994f199be
KTZimmerle <kzimmerl@uci.edu>
2016-04-08 23:35:44 -0700

    Scripts for movement and boundary
    Boundary code or objects not set up correctly

---
+++ Assets/Animal_DestroybyBoundary.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animal_DestroybyBoundary : MonoBehaviour {
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("out");
+
+        Destroy(other.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Inside");
+    }
+}
